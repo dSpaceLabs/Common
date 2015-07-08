d47c755d5d634e71bc8214814f423807e3f40093
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2015-07-07 21:54:27 -0400

    added another trait because I am lazy

---
+++ src/Model/TimestampAwareTrait.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ */
+
+namespace Dspacelabs\Common\Model;
+
+/**
+ */
+trait TimestampAwareTrait
+{
+    /**
+     * @var \DateTime
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime
+     */
+    protected $updatedAt;
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
